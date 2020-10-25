83bbd57fd2283af17805ef801a5c7fc0efe60f86
Kundjanasith Thonglek <kundjanasith.t@ku.th>
2020-10-25 17:39:15 +0900

    add face

--- prep/camera.py
+++ prep/camera.py
@@ -18,5 +18,17 @@ class VideoCamera(object):
         # We are using Motion JPEG, but OpenCV defaults to capture raw images,
         # so we must encode it into JPEG in order to correctly display the
         # video stream.
+        faceCascade = cv2.CascadeClassifier('../../haarcascade_frontalface_default.xml')
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE
+        )
+        for (x, y, w, h) in faces:
+            cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
         ret, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
